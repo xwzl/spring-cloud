9e5bee8ddb490cd120913f156499c098f5ffd613
xuweizhi <158262751>
2019-07-15 16:18:48 +0800

    项目初始化

---
+++ spring-boot/src/test/java/com/java/boot/SpringBootsTest.java
@@ -0,0 +1,12 @@
+package com.java.boot;
+
+/**
+ * @author xuweizhi
+ * @date 2019/04/23 15:44
+ */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SpringBootsTest {
+
+}
+
---
+++ spring-boot/src/test/java/com/java/boot/redis/PeopleServiceTest.java
@@ -0,0 +1,19 @@
+package com.java.boot.redis;
+
+import com.java.boot.SpringBootsTest;
+
+/**
+ * @author xuweizhi
+ * @date 2019/04/23 16:28
+ */
+public class PeopleServiceTest extends SpringBootsTest {
+
+    //@Autowired
+    //private PeopleService peopleService;
+    //
+    //@Test
+    //public void getUser() {
+    //    People byId = peopleService.findById(5);
+    //    System.out.println(byId);
+    //}
+}
