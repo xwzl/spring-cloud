bde836e4f1730a78baa7928e3cac8785e0afb188
xuweizhi <158262751>
2019-07-29 15:22:24 +0800

    Swagger2

--- spring-demo/src/main/java/com/spring/demo/model/Role.java
+++ spring-demo/src/main/java/com/spring/demo/model/Role.java
@@ -1,17 +1,20 @@
 package com.spring.demo.model;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import java.io.Serializable;
+
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author xuweizhi
@@ -21,21 +24,28 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("role")
+@ApiModel(description = "角色实体")
 public class Role implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(value = "id")
     private Integer id;
 
     @TableField("name")
+    @ApiModelProperty(value = "姓名")
     private String name;
 
     /**
      * 角色名称
      */
     @TableField("nameZh")
+    @ApiModelProperty(value = "角色名称")
     private String nameZh;
 
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
 
 }
