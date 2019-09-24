3890712249cdbd75ef2b3f24edb55f72c15a951d
徐伟智 <l>
2019-09-24 18:13:49 +0800

    RedisTemplate 设置自增，但是类型为 integer

--- spring-demo/src/main/java/com/spring/demo/controller/RedisController.java
+++ spring-demo/src/main/java/com/spring/demo/controller/RedisController.java
@@ -2,23 +2,28 @@ package com.spring.demo.controller;
 
 
 import com.spring.demo.model.dos.People;
+import com.spring.demo.model.vos.ApiResult;
 import com.spring.demo.service.PeopleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
- * 前端控制器
+ * redis 测试
  *
  * @author xwz
  * @since 2019-04-22
  */
+@Slf4j
 @RestController
 @RequestMapping("/user")
 @Api(tags = "redis 缓存测试，注解版本")
@@ -26,6 +31,9 @@ public class RedisController {
 
     @Autowired
     private PeopleService peopleService;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @ApiOperation(value = "Master 插入值", notes = "hello接口")
     @GetMapping("/insertUser")
@@ -60,5 +68,29 @@ public class RedisController {
     @ApiOperation(value = "getPlus", notes = "hello接口")
     public People getPlus(Integer id) {
         return peopleService.getById(id);
+    }
+
+    @GetMapping("/string")
+    public ApiResult<Object> addString(String key) {
+
+        ValueOperations<String, Object> value = redisTemplate.opsForValue();
+        value.set(key, "键值对测试");
+        if (value.get(key) != null) {
+            log.info(Objects.requireNonNull(value.get(key)).toString());
+        }
+
+        // 如果存在这个建，就添加吗？ 如果存在，则不覆盖
+        value.setIfAbsent(key + 1, "测试");
+        log.info(Objects.requireNonNull(value.get(key + 1)).toString());
+
+        value.setIfAbsent(key, "测试");
+        log.info(Objects.requireNonNull(value.get(key)).toString());
+
+        value.set(key, 1);
+        log.info(Objects.requireNonNull(value.get(key)).toString());
+
+        value.increment(key);
+        log.info(Objects.requireNonNull(value.get(key)).toString());
+        return null;
     }
 }
