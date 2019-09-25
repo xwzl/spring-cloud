ae21537c7646297436310849f3de44cbd766541d
徐伟智 <624244232@qq.com>
2019-09-25 23:26:59 +0800

    SpringBoot 启动完成后打印那个 swagger 地址

---
+++ spring-demo/src/main/java/com/spring/demo/config/ApplicationRunnerAfter.java
@@ -0,0 +1,32 @@
+package com.spring.demo.config;
+
+import com.spring.demo.untils.IpAddressUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * SpringBoot 完成后打印 swagger 文档地址
+ *
+ * @author xuweizhi
+ * @date 2019/09/25 22:44
+ */
+@Order(100)
+@Component
+@Slf4j
+public class ApplicationRunnerAfter implements ApplicationRunner {
+
+    @Autowired
+    private IpConfiguration ipConfiguration;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("Swagger github 文档地址: http://" + IpAddressUtils.getHostAddress() + ":" + ipConfiguration.getPort() + "/swagger-ui.html");
+        log.info("Swagger github boot-strap: http://" + IpAddressUtils.getHostAddress() + ":" + ipConfiguration.getPort() + "/doc.html");
+    }
+
+
+}
