537c399e8d17a22b0ef649f5b107f745546c7410
徐伟智 <l>
2019-10-29 17:51:32 +0800

    字符串反转

---
+++ spring-base/src/main/java/com/spring/base/java/container/StringReverse.java
@@ -0,0 +1,34 @@
+package com.spring.base.java.container;
+
+import lombok.Data;
+import org.jetbrains.annotations.Contract;
+
+/**
+ * 字符串逆序
+ *
+ * @author xuweizhi
+ * @since 2019/10/29 17:18
+ */
+@Data
+public class StringReverse {
+
+    private String content;
+
+    @Contract(pure = true)
+    public StringReverse(String content) {
+        this.content = content;
+    }
+
+    public String reverse() {
+        char[] chars = this.content.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = chars.length; i > 0; i--) {
+            sb.append(chars[i - 1]);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args){
+        System.out.println(new StringReverse("Hello Java !").reverse() );
+    }
+}
