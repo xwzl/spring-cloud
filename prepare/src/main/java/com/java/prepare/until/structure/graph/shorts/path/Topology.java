693b465452860370d9f08d7dc1f8b150e2e5a2e1
xuweizhi <624244232@qq.com>
2020-06-03 18:29:08 +0800

    数据结构：拓扑排序

---
+++ prepare/src/main/java/com/java/prepare/until/structure/graph/shorts/path/Topology.java
@@ -0,0 +1,20 @@
+package com.java.prepare.until.structure.graph.shorts.path;
+
+/**
+ * 拓扑排序
+ *
+ * 在一个表示工程的有向图中，用顶点表示活动，用弧表示活动之间的有限关系，这样的有向图
+ * 为顶点表示的网，我们成为 AOV 网（Activity On Vertex Network）.AOV 网中的弧表示
+ * 活动之间存在的某种制约关系.
+ *
+ * 设 G=(V,E) 是一个具有 n 个顶点的有向图，v 中的顶点序列 v1,v2,......vn,满足若从
+ * 顶点 vi 到 vj 有一条路径，则在顶点序列中顶点 vi 必在顶点 vj 之前。则我们称这样的
+ * 顶点序列为一个拓扑排序。
+ *
+ * 所谓拓扑排序，其实就是对一个有向图构造
+ *
+ * @author xuweizhi
+ * @since 2020/06/03 18:02
+ */
+public class Topology {
+}
