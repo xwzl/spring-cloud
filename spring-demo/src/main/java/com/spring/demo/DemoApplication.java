961d5f005b28ee1eec7b93c02a67da0101732b82
xuwezhi <hanlin.xwz@come-future.com>
2021-06-01 18:15:26 +0800

    Spring 事件机制

--- spring-demo/src/main/java/com/spring/demo/DemoApplication.java
+++ spring-demo/src/main/java/com/spring/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * @author xuweizhi
  * @since 2019/06/28 13:43
@@ -29,21 +32,21 @@ public class DemoApplication {
      */
     @SuppressWarnings("unchecked")
     private static void disableAccessWarnings() {
-        //try {
-        //    Class unsafeClass = Class.forName("sun.misc.Unsafe");
-        //    Field field = unsafeClass.getDeclaredField("theUnsafe");
-        //    field.setAccessible(true);
-        //    Object unsafe = field.get(null);
-        //
-        //    Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
-        //    Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
-        //
-        //    Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
-        //    Field loggerField = loggerClass.getDeclaredField("logger");
-        //    Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
-        //    putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
-        //} catch (Exception ignored) {
-        //}
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        } catch (Exception ignored) {
+        }
     }
 
 }
